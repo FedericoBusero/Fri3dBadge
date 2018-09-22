f2fc83ce63816bcfcf562342b270203b2ba10779
FedericoBusero <35894905+FedericoBusero@users.noreply.github.com>
2018-09-22 19:19:35 +0200

    Update defaultFirmware.ino

--- examples/2.Demos/defaultFirmware/defaultFirmware.ino
+++ examples/2.Demos/defaultFirmware/defaultFirmware.ino
@@ -4,13 +4,15 @@
 
 #include "Fri3dMatrix.h"
 #include "Fri3dAccelerometer.h"
-#include "Fri3dBuzzer.h"
-#include "Fri3dButtons.h"
+#include "Buzzer.h"
+#include "Button.h"
+#include "Fri3dConfig.h"
 
 Fri3dAccelerometer accel = Fri3dAccelerometer();
 Fri3dMatrix matrix = Fri3dMatrix();
-Fri3dBuzzer buzzer = Fri3dBuzzer();
-Fri3dButtons buttons = Fri3dButtons();
+Buzzer buzzer = Buzzer(BUZZER_PIN, 0);
+Button button0    = Button(BUTTON0_PIN);
+Button button1    = Button(BUTTON1_PIN);
 
 // state machine
 
@@ -18,13 +20,13 @@ enum State { STATE_EYES, STATE_TEXT };
 State state = STATE_EYES;
 
 void nextState() {
-  switch( state ) {
+  switch ( state ) {
     case STATE_EYES:
       state = STATE_TEXT;
-    break;
+      break;
     case STATE_TEXT:
       state = STATE_EYES;
-    break;
+      break;
   }
 }
 
@@ -37,12 +39,12 @@ void nextState() {
 #define SERVICE_UUID        "111fc111-1fb5-459e-8fcc-c5c9c331914b"
 #define CHARACTERISTIC_UUID "11154111-36e1-4688-b7f5-ea07361b26a8"
 
-uint64_t chipid; 
+uint64_t chipid;
 
 void startBLEBeacon() {
-    Serial.println("===== Setup the Beacon... =====");
+  Serial.println("===== Setup the Beacon... =====");
 
-  chipid = chipid=ESP.getEfuseMac();
+  chipid = chipid = ESP.getEfuseMac();
 
   String fri3d = "fri3d";
   String fri3dmac = fri3d + String((uint32_t)chipid);
@@ -69,19 +71,18 @@ void startBLEBeacon() {
 
 void setup() {
   Serial.begin(9600);
-  
+
   accel.begin();
-  
+
   buzzer.setVolume( 255 );
-  buzzer.setFrequency( 600 );
+  buzzer.tone( 600 );
   delay( 120 );
-  buzzer.setFrequency( 500 );
+  buzzer.tone( 500 );
   delay( 120 );
-  buzzer.setVolume(0);
-  
-  buttons.setReleasedCallback( 0, nextState );
-  buttons.setReleasedCallback( 1, nextState );
-  buttons.startDebounceThread();
+  buzzer.noTone();
+
+  button0.setReleasedCallback( nextState );
+  button1.setReleasedCallback( nextState );
 
   startBLEBeacon();
 }
@@ -92,24 +93,24 @@ int frame = 0;
 int frame_blink = 40;
 void renderFoxEyes() {
   // render eyes based on accelerometer
-  sensors_event_t event; 
+  sensors_event_t event;
   accel.getEvent(&event);
   matrix.clear();
   int x = 1 + 5 * ( event.acceleration.y + 12 ) / 24;
-  if( frame_blink - 2 > frame ) {
+  if ( frame_blink - 2 > frame ) {
     matrix.setPixel( x    , 0, 1 );
     matrix.setPixel( x + 7, 0, 1 );
     matrix.setPixel( x    , 4, 1 );
     matrix.setPixel( x + 7, 4, 1 );
   }
-  if( frame_blink - 1 > frame ) {
+  if ( frame_blink - 1 > frame ) {
     matrix.setPixel( x - 1, 1, 1 );
     matrix.setPixel( x    , 1, 1 );
     matrix.setPixel( x + 1, 1, 1 );
     matrix.setPixel( x + 6, 1, 1 );
     matrix.setPixel( x + 7, 1, 1 );
     matrix.setPixel( x + 8, 1, 1 );
-    
+
     matrix.setPixel( x - 1, 2, 1 );
     matrix.setPixel( x + 1, 2, 1 );
     matrix.setPixel( x + 6, 2, 1 );
@@ -122,7 +123,7 @@ void renderFoxEyes() {
     matrix.setPixel( x + 7, 3, 1 );
     matrix.setPixel( x + 8, 3, 1 );
   }
-  if( frame_blink == frame ) {
+  if ( frame_blink == frame ) {
     matrix.setPixel( x - 1, 2, 1 );
     matrix.setPixel( x    , 2, 1 );
     matrix.setPixel( x + 1, 2, 1 );
@@ -132,7 +133,7 @@ void renderFoxEyes() {
     frame_blink = frame_blink + rand() % 90 + 30;
   }
   frame++;
-  if( frame > frame_blink )
+  if ( frame > frame_blink )
     frame_blink = frame + rand() % 90 + 30;
   delay(50);
 }
@@ -141,7 +142,7 @@ void renderFoxEyes() {
 
 String s = "FRI3D CAMP 2018";
 void renderText() {
-  if( frame >= (int)( s.length() ) * 4 )
+  if ( frame >= (int)( s.length() ) * 4 )
     frame = -15;
   matrix.clear();
   matrix.drawString( -frame, s );
@@ -152,12 +153,12 @@ void renderText() {
 // PIXEL TEST
 
 void renderPixels() {
-  if( frame < 70 )
+  if ( frame < 70 )
     matrix.setPixel( frame % 14, frame / 14, 1 );
   else
     matrix.setPixel( ( frame - 70 ) % 14, ( frame - 70 ) / 14, 0 );
   frame++;
-  if( frame >= 140 ) {
+  if ( frame >= 140 ) {
     frame = 0;
   }
   delay(10);
@@ -166,15 +167,19 @@ void renderPixels() {
 // MAIN LOOP
 
 void loop() {
-  if( buttons.getDebouncedState(0) )
+  button0.debounceButton(  );
+  button1.debounceButton(  );
+
+  if ( button0.getDebouncedState() )
     renderPixels();
   else
-    switch(state) {
-      case STATE_EYES: 
+    switch (state) {
+      case STATE_EYES:
         renderFoxEyes();
-      break;
-      case STATE_TEXT: 
-        renderText(); 
-      break;
+        break;
+      case STATE_TEXT:
+        renderText();
+        break;
     }
+
 }
